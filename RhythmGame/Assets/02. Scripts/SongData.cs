4fdc3094afc339f6047ed3090755ec770f531b5c
kmdoyeon <98201701+kmdoyeon@users.noreply.github.com>
2022-05-07 13:15:32 +0900

    리듬겜

---
+++ RhythmGame/Assets/02. Scripts/SongData.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+
+[System.Serializable]
+public class SongData
+{
+    public string videoName;
+    public List<NoteData> notes;
+
+    public SongData()
+    {
+        notes = new List<NoteData>();
+    } 
+
+}
